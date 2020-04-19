1f82116b479bd04bce6be5e078c44be9f294cd6d
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-04-19 22:36:57 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  new_array = ["water bottle", "foam roller", "running shoes"]
-  puts new_array << "crowd"
+  array << element
 end
 
 def add_element_to_start_of_array(array, element)
